24fa4c1b4614ddba4123eedeebfffa92062cacba
KFlash <YOUR EMAIL ADDRESS>
2016-06-07 13:47:50 +0800

    v.5.1.8

---
+++ dist/boily.js
@@ -0,0 +1,25 @@
+/*!
+ * boily v5.1.8
+ * (c) 2016 KFlash
+ * Released under the MIT License.
+ */
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.Boily = factory());
+}(this, function () { 'use strict';
+
+	var boily = { foo: 123 };
+
+	// Correct version will be set with the 'rollup-replace plugin'
+	boily.version = 'VERSION';
+
+	// Flow example
+	function foo(one, two, three) {}
+
+	boily.flow = foo;
+
+	return boily;
+
+}));
+//# sourceMappingURL=boily.js.map
